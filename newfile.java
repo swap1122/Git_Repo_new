73ff527c11eb40a66b3fb68f7067fc03065a7536
swap1122 <swapnilsonwane1122@gmail.com>
2023-05-06 00:23:25 +0530

    new

---
+++ newfile.java
@@ -0,0 +1,14 @@
+//import java.util.*;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		//Scanner sc=new Scanner(System.in);
+		System.out.println("enter mark");
+         int mark=40;//sc.nextInt();
+         String result = (mark > 40) ? "pass" : "fail" ;
+         System.out.println("your mark is "+mark+" and you are "+result+" in exam");
+	}
+
+}
+
